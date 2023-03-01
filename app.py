c62a8487f925a0fcb8e93793cb06750d5db2288e
venkysubby <91669076+venkysubby@users.noreply.github.com>
2023-03-01 12:45:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "my first docker app"
+    name = request.args.get('name', 'World"
+    return f'Hello,  {name}!'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
